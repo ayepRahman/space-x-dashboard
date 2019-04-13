83bd19695142bdc9fdc3e1bca558c982cb791644
arifrahman <matrep88@gmail.com>
2019-04-14 02:19:42 +0800

    added todo for dashboard list

---
+++ src/ui/graphql/LaunchPastList/index.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+// Api params - https://docs.spacexdata.com/#bc65ba60-decf-4289-bb04-4ca9df01b9c1
+
+/**
+ * TODO:
+ * [] Add table
+ * [] Add Query component
+ * [] Add handlePageNumber - offset by increment by 10
+ * [] Add handleSort - [sort]: "key", [order]: "asc" || "desc"
+ * [] loading in table row ?
+ */
+
+const LaunchPastList = () => {
+  return <div />;
+};
+
+export default LaunchPastList;
