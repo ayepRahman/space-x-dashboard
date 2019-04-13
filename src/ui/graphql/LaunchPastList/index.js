4f784947275705c4cfdb1b8abe2d68444beaf5a2
arifrahman <matrep88@gmail.com>
2019-04-14 02:55:19 +0800

    added handler for offset,sort,order

--- src/ui/graphql/LaunchPastList/index.js
+++ src/ui/graphql/LaunchPastList/index.js
@@ -1,4 +1,7 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { withApollo } from 'react-apollo';
+
+import { Table, Button } from 'antd';
 
 // Api params - https://docs.spacexdata.com/#bc65ba60-decf-4289-bb04-4ca9df01b9c1
 
@@ -12,6 +15,37 @@ import React from 'react';
  */
 
 const LaunchPastList = () => {
+  const [state, setState] = useState({
+    limit: 10,
+    offSet: 10,
+    sort: '',
+    order: '',
+  });
+
+  const handleOffset = offSet => {
+    setState({
+      ...state,
+      offSet,
+      sort: '',
+      order: '',
+    });
+  };
+
+  const handleSort = (sort, order) => {
+    setState({
+      ...state,
+      sort,
+      order,
+    });
+
+    getLaunchesPast();
+  };
+
+  const getLaunchesPast = () => {
+    try {
+    } catch (error) {}
+  };
+
   return <div />;
 };
 
